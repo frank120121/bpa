b36e0ccbbe546bc5bf9c3bd8482185b79358b14c
frank120121 <francisco.lopez4256@gmail.com>
2024-03-13 14:56:59 -0700

    Update database table 'orders' to include 'user_bank' column

--- binance_db.py
+++ binance_db.py
@@ -165,8 +165,8 @@ async def main():
         # Print table contents for verification
         # await remove(conn, '20598203477247664128')
         # await remove_user(conn, 'MARTINEZ MARTINEZ JUAN MANUEL')
-        await add_column_if_not_exists(conn, 'orders', 'buyer_bank', 'TEXT', 'NULL')
-        await print_table_contents(conn, 'orders')
+        await add_column_if_not_exists(conn, 'users', 'user_bank', 'TEXT', 'NULL')
+        await print_table_contents(conn, 'users')
 
         await conn.close()
     else:
